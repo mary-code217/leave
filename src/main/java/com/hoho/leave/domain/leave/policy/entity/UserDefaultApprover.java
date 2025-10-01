4cf1a8e4a60ee996a093c7f32e1fc59134561abe
xorl0514 <xorl0514@naver.com>
2025-10-01 15:37:58 +0900

    feat: defaultApprover 도메인 구현

--- src/main/java/com/hoho/leave/domain/leave/policy/entity/UserDefaultApprover.java
+++ src/main/java/com/hoho/leave/domain/leave/policy/entity/UserDefaultApprover.java
@@ -3,7 +3,9 @@ package com.hoho.leave.domain.leave.policy.entity;
 import com.hoho.leave.config.jpa.BaseEntity;
 import com.hoho.leave.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -13,6 +15,7 @@ import lombok.Getter;
                 @UniqueConstraint(name = "uq_uda_user_step", columnNames = {"user_id", "step_no"})
         }
 )
+@NoArgsConstructor
 public class UserDefaultApprover extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,4 +30,18 @@ public class UserDefaultApprover extends BaseEntity {
 
     @Column(name = "step_no", nullable = false)
     private Integer stepNo;
+
+    @Builder
+    public UserDefaultApprover(User user, User approver, Integer stepNo) {
+        this.user = user;
+        this.approver = approver;
+        this.stepNo = stepNo;
+    }
+
+    public static UserDefaultApprover of(User user, User approver, Integer stepNo) {
+        return UserDefaultApprover.builder()
+                .user(user)
+                .approver(approver)
+                .stepNo(stepNo).build();
+    }
 }
---
+++ src/main/java/com/hoho/leave/domain/leave/policy/service/UserDefaultApproverService.java
@@ -0,0 +1,24 @@
+package com.hoho.leave.domain.leave.policy.service;
+
+import com.hoho.leave.domain.leave.policy.dto.request.UserDefaultApproverCreate;
+import com.hoho.leave.domain.leave.policy.entity.UserDefaultApprover;
+import com.hoho.leave.domain.leave.policy.repository.UserDefaultApproverRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDefaultApproverService {
+
+    private final UserDefaultApproverRepository defaultApproverRepository;
+
+    @Transactional
+    public UserDefaultApprover createUserDefaultApprover(UserDefaultApproverCreate dto) {
+
+
+
+
+        return null;
+    }
+}
